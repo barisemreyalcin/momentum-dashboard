19d674f9328501bd48081a7ae1be5376a8643e3f
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-08-26 21:51:38 +0300

    display the author on the page

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const authorEl = document.getElementById("author");
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=view")
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        document.body.style.backgroundImage = `url(${data.urls.full})`;
+        document.body.style.backgroundImage = `url(${data.urls.regular})`;
+        authorEl.textContent = `By: ${data.user.name}`;
     })
